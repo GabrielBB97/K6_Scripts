9a5d96f1ec248d2b71b3bee21ac5f059d628422a
Gabriel Bustamante <gabriel.beltran.jgbb@hotmail.com>
2025-05-19 15:57:21 -0600

    Updating runtime settings.js and adding k6 run commands.txt

--- 2024/BlazeDemo_Flights/Runtime_Settings.js
+++ 2024/BlazeDemo_Flights/Runtime_Settings.js
@@ -23,6 +23,24 @@ return {
 			timeUnit: '1s',
 			preAllocatedVUs: 7,
 			maxVUs: 10
+
+		/*
+
+		executor: 'ramping-arrival-rate',
+
+		startRate: 5,
+		timeUnit: '1s',
+		preAllocatedVUs: 5,
+		maxVUs: 100,
+		stages: [
+			{target: 5, duration: '10m'},
+			{target: 20, duration: '20m'},
+			{target: 35, duration: '30m'},
+			{target : 5, duration: '5m'},
+			{target: 0, duration: '1m'},
+		]
+
+		*/
 			
 
 		}
